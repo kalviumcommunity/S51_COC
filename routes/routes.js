07d745641c1fc39b5833fd0af2d4d73d56e31876
Prasanth <87628858+PrasanthYT@users.noreply.github.com>
2024-03-05 16:54:36 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -81,12 +81,6 @@ patchRouter.patch("/patch/:captionId", async (req, res) => {
 
     const validationResult = updateAndPostValidator(updates);
 
-    if (validationResult.error) {
-      const errors = validationResult.error.details.map(detail => detail.message);
-      console.error('Validation Error:', errors);
-      return res.status(400).json({ error: 'Validation Error', details: errors });
-    }
-
     const caption = await Captions.findOneAndUpdate(
       { captionID: captionId },
       { $set: updates },
